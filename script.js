5d36ef3defe3c2a7fdbe66b940b2f7836c7d4081
raghoul <82054167+raghoul@users.noreply.github.com>
2021-06-01 09:46:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,208 +1,134 @@
-// Fonctionnalité 1 :
-
-let footer = document.querySelector("footer");
-
-function posterOnTheConsole()
-{
-    console.log("clique");
+//function 1-bis
+let myFooter = document.querySelector("footer");
+let i = 1;
+function footerEffect(){
+  console.log(`Click number ${i}.`);
+  i++;
 }
 
-footer.addEventListener("click",posterOnTheConsole);
+myFooter.addEventListener("click",footerEffect);
 
 
-
-
-// Fonctionnalité 1bis :
-
-let footerbis = document.querySelector("footer");
-let counter = 1;
-
-function posterOnTheConsoleBis()
-{
-    console.log(`clique ${counter}`);
-    counter++;
+// function 2
+const toggler = () => {
+    navbarHeader.classList.toggle('collapse');
 }
+const menu = document.querySelector('button').addEventListener('click', toggler);
 
-footerbis.addEventListener("click",posterOnTheConsoleBis);
 
-
-
-
-
-// Fonctionnalité 2 :
-
-function hamburgerMenu() {
-	nav = document.getElementById("navbarHeader")
-	hambuger = document.querySelector("button", ".navbar-toggler")
-	hambuger.addEventListener("click", function () {
-		nav.classList.toggle("collapse")
-	})
-}
-
-hamburgerMenu()
-
-
-
-
-
-
-// Fonctionnalité 3 :
-
-
-function redText() {
-    editCardButtonOne = document.querySelectorAll("div.col-md-4")[0]
-    textCardOne = document.querySelectorAll("div.col-md-4 button.btn-outline-secondary")[0]
-    editCardButtonOne.addEventListener("click", function() {
-    textCardOne.style.color = "red"
-    })
-}
-redText()
-
-
-
-
-
-
-// Fonctionnalité 4 :
-
-
-function greenText() {
-	editCardButtonTwo = document.querySelectorAll("div.col-md-4")[1]
-	textCardTwo = document.querySelectorAll("div.col-md-4 button.btn-outline-secondary")[1]
-	i = 1
-	editCardButtonTwo.addEventListener("click", function() {
-		i++
-    if (textCardTwo.style.color === "green")
-    {
-      textCardTwo.style.color = "";
-    }
-    else
-    {
-      textCardTwo.style.color = "green";
-    }
-	})
-}
-greenText()
-console.log("")
-
-
-
-
-
-// Fonctionnalité 5 :
-
-
-function nuclearPage() {
-	navbar = document.querySelector("div.navbar")
-	bootstrap = document.querySelector("head").querySelector("link")
-	i = 1
-	navbar.addEventListener("dblclick", function () {
-		i++
-		if (document.getElementsByTagName("link")[0].rel == "stylesheet")           //if stylesheet is on, we turn it off
-    document.getElementsByTagName("link")[0].rel = "styleDisabled";
-  else
-    document.getElementsByTagName("link")[0].rel = "stylesheet";
-	})
-}
-
-nuclearPage()
-console.log("")
-
-
-
-
-
-
-
-// Fonctionnalité 6 :
-
-
-let cards = document.getElementsByClassName("card");
-let button_view = document.getElementsByClassName("btn-success");
-
-for(let i = 0; i < cards.length; i++)
-{
-    function view()
-    {
-        if (cards[i].getElementsByClassName("card-text")[0].style.display != "none")
-        {
-            cards[i].getElementsByClassName("card-text")[0].style.display = "none";
-            cards[i].getElementsByClassName("card-img-top")[0].style.width = "20%";
-        }
-        else
-        {
-            cards[i].getElementsByClassName("card-text")[0].style.display = "block";
-            cards[i].getElementsByClassName("card-img-top")[0].style.width = "100%";
-        }
-    }
-    button_view[i].addEventListener("mouseover", view);
-}
-console.log("")
-
-
-
-
-
-
-
-
-
-// Fonctionnalité 7 :
-
-
-const buttonNext = document.querySelector('main section p a.btn-secondary'); 
-const cards2 = document.querySelectorAll('div.col-md-4'); 
-const divcards = document.querySelector('div.album div.container div.row');
-let card_1 = divcards.children[0];
-let card_6 = divcards.children[5];
-
-buttonNext.addEventListener('click', function() {   
-  divcards.insertBefore(card_6, card_1);
-  card_1 = divcards.children[0];
-  card_6 = divcards.children[5];
-}); 
-
-
-
-
-
-// Fonctionnalité 8 :
-
-
-const buttonPrevious = document.querySelector('main section p a.btn-primary'); 
-card_1 = divcards.children[0];
-card_6 = divcards.children[5];
-
-buttonPrevious.addEventListener('click', function(event) {   
-  event.preventDefault();            
-  divcards.insertBefore(card_1, card_6.nextElementSibling);
-  card_1 = divcards.children[0];
-  card_6 = divcards.children[5];
-}); 
-
-
-
-
-
-//Fonctionnalité 9
-
-
-let logo = document.getElementsByClassName('navbar-brand')[0];
-logo.addEventListener('keypress', function(e) {
-  let body = document.getElementsByTagName('body')[0];
-  body.classList = "";
-  switch (e.key) {
-    case 'a':
-      body.classList.add("col-4");
-      break;
-    case 'b':
-      body.classList = "";
-      break;
-    case 'y':
-      body.classList.add("col-4", "offset-4");
-      break;
-    case 'p':
-      body.classList.add("col-4", "offset-8");
-      break;
-  }
+// function 3
+document.getElementsByClassName("btn-outline-secondary")[0].addEventListener('click', () => {
+    document.getElementsByClassName("card-text")[0].style.color = 'red'
 });
+
+
+// function 4
+const textCard = document.getElementsByClassName("card-text")[1];
+let btnBool = false;
+const editButton = document.getElementsByClassName("btn-outline-secondary")[1];
+
+editButton.addEventListener('click', () => {
+    if (btnBool) {
+        btnBool = false;
+        textCard.style.color = '';
+    } else {
+        btnBool = true;
+        textCard.style.color = 'green';
+    }
+});
+
+
+// function 5
+let navBar = document.getElementsByTagName("header")[0];
+let targetLink = document.getElementsByTagName("link")[0];
+// so far I understood only a link tag can be disabled
+// whereas an href can't apply disabled method
+function toggleBootstrap(){
+  if (targetLink.disabled === true){
+    targetLink.disabled = false;
+  } else {
+    targetLink.disabled = true;
+  }
+}
+
+navBar.addEventListener("dblclick",toggleBootstrap);
+
+
+// function 6
+// won't be messed up by card mouvements
+// the solution is NOT USING index
+// simple collection iteration such as foreach
+// or more complicated logics such as, finding the button by index
+// and other stuff by parentElement
+// make things work
+function hideCard(btn){
+  var card = btn.target.closest('.card');
+  var image = card.getElementsByClassName("card-img-top")[0];
+  var text = card.getElementsByClassName("card-text")[0];
+  var hover = text.style.visibility == 'hidden'
+
+  if(hover){
+    text.style= "";
+    image.style = "";
+    hover = false;
+  } else {
+    text.style= "visibility:hidden";
+    image.style = "width:20%;";
+    hover = true;
+  }
+}
+
+let buttons = document.getElementsByClassName("btn-success");
+for(let i = 0; i < buttons.length; i++){
+  var viewBtn = buttons[i];
+  //when the function is defined with an argument
+  // and followed by false, it works
+  viewBtn.addEventListener("mouseover", hideCard, false)
+}
+
+//* function 7
+// no, didn't manage to fix the lag T_T
+let blocks = document.getElementsByClassName("col-md-4");
+
+let switchBtn1 = document.getElementsByClassName("my-2")[1];
+let album = document.getElementsByClassName("row")[1];
+function switchCards(){
+  album.insertBefore(blocks[blocks.length-1], blocks[0]);
+}
+
+switchBtn1.addEventListener("click",switchCards);
+
+
+// function 8
+// difference between disabling a link tag
+// and disabling the href of an a tag :
+// for the former, a simple disabled = true
+// for the latter, need to removeAttribute("href")
+let switchBtn2 = document.getElementsByClassName("btn-primary")[0];
+let externalLink = document.getElementsByTagName("a")[4]
+externalLink.removeAttribute("href");
+function switchBack(){
+  album.insertBefore(blocks[0], blocks[blocks.length]);
+}
+// note how it is possible to insert before a fictive "last element" that doesn't exist
+switchBtn2.addEventListener("click",switchBack);
+
+// function 9
+let jsEvent = document.querySelector('.navbar-brand')
+let body = document.querySelector('body')
+
+jsEvent.addEventListener('keydown', (press) => {
+    if (press.key == "a")
+      body.setAttribute('class', 'col-4')
+
+    else if (press.key == "y")
+       body.setAttribute("class",'col-4 offset-4')
+
+    else if (press.key == "p")
+    body.setAttribute("class",'col-4 offset-8')
+
+    else if (press.key == "b")
+    body.removeAttribute("class")
+
+    
+  });
